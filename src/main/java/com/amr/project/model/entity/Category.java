cf5be9076dd98b45159e23926fe32509da5db27a
Artem Zakharov <zart208@mail.ru>
2021-09-04 19:58:39 +0300

    fix entities with @Builder annotation for project run without errors

--- src/main/java/com/amr/project/model/entity/Category.java
+++ src/main/java/com/amr/project/model/entity/Category.java
@@ -1,5 +1,6 @@
 package com.amr.project.model.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "category")
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Builder
 public class Category {
