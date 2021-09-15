7c89e7aa174f87df77d968643f78056b5b25736d
Yefim Savin <savin.efim2@gmail.com>
2021-09-15 16:52:02 +0500

    add verification by email to user

--- src/main/java/com/amr/project/service/impl/UserServiceImpl.java
+++ src/main/java/com/amr/project/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.amr.project.service.impl;
 import com.amr.project.dao.abstracts.UserDao;
 import com.amr.project.model.entity.User;
 import com.amr.project.service.abstracts.UserService;
+import com.amr.project.service.email.EmailVerificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,11 +24,13 @@ public class UserServiceImpl extends ReadWriteServiceImpl<User, Long> implements
 
     private final UserDao userDAO;
     private PasswordEncoder passwordEncoder;
+    private EmailVerificationService verificationService;
 
     @Autowired
-    public UserServiceImpl(UserDao userDAO) {
+    public UserServiceImpl(UserDao userDAO, EmailVerificationService verificationService) {
         super(userDAO);
         this.userDAO = userDAO;
+        this.verificationService = verificationService;
         passwordEncoder = new BCryptPasswordEncoder();
     }
 
@@ -54,6 +57,8 @@ public class UserServiceImpl extends ReadWriteServiceImpl<User, Long> implements
     public void persist(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userDAO.persist(user);
+        verificationService.sendVerificationEmail(user);
+
     }
 
 }
