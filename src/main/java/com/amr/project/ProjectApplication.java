0b2be8438b444fd3d7a3fdde3220c8054ee9d178
Dima Tereschenko <dimanorgitlab@gmail.com>
2021-09-23 16:08:42 +0700

    TrackedEmail 1.3

--- src/main/java/com/amr/project/ProjectApplication.java
+++ src/main/java/com/amr/project/ProjectApplication.java
@@ -1,26 +1,10 @@
 package com.amr.project;
 
-import com.amr.project.converter.ItemMapper;
-import com.amr.project.model.dto.ItemDto;
-import com.amr.project.model.entity.Category;
-import com.amr.project.model.entity.Item;
-import com.amr.project.model.entity.Review;
-import com.amr.project.model.entity.Shop;
-import com.amr.project.service.impl.CategoryServiceImpl;
-import com.amr.project.service.impl.ItemServiceImpl;
-import com.amr.project.service.impl.ReviewServiceImpl;
-import com.amr.project.service.impl.ShopServiceImpl;
-import com.amr.project.webapp.rest_controller.ItemRestController;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
-
-import javax.annotation.PostConstruct;
-import java.math.BigDecimal;
-
 
 @SpringBootApplication(exclude = {SessionAutoConfiguration.class, SecurityAutoConfiguration.class})
 @EnableAsync
