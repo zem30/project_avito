07c45a3effcc9f73fbc82112095d45eec0c2d5bb
Zelimkhan Yandarbiev <tagaev77777@mail.ru>
2021-09-24 12:31:53 +0000

    Update src/main/java/com/amr/project/webapp/controller/HomePageController.java

--- src/main/java/com/amr/project/webapp/controller/HomePageController.java
+++ src/main/java/com/amr/project/webapp/controller/HomePageController.java
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
