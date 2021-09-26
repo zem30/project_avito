ce0c45aa9c66c1708c487098e432763f61f9f805
Leonid Simukov <pcjmeil@gmail.com>
2021-09-26 16:42:31 +0300

    Change orders status. Step 3:
    Changelog:
    1. Fixed merge conflicts

--- src/main/java/com/amr/project/service/impl/OrderServiceImpl.java
+++ src/main/java/com/amr/project/service/impl/OrderServiceImpl.java
@@ -3,47 +3,75 @@ package com.amr.project.service.impl;
 import com.amr.project.dao.impl.OrderDaoImpl;
 import com.amr.project.model.entity.Mail;
 import com.amr.project.model.entity.Order;
+import com.amr.project.model.enums.Status;
+import com.amr.project.service.abstracts.OrderService;
 import com.amr.project.service.email.EmailSenderService;
 import com.amr.project.util.TrackedEmailOrder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class OrderServiceImpl extends ReadWriteServiceImpl<Order, Long> {
+@Transactional
+public class OrderServiceImpl extends ReadWriteServiceImpl<Order, Long> implements OrderService {
 
-    private final OrderDaoImpl orderDaol;
-
+    private final OrderDaoImpl orderDao;
     private final EmailSenderService emailSenderService;
-
     private final TrackedEmailOrder trackedEmailOrder;
 
-    protected OrderServiceImpl(OrderDaoImpl dao, OrderDaoImpl orderDaol, EmailSenderService emailSenderService, TrackedEmailOrder trackedEmailOrder) {
+    protected OrderServiceImpl(OrderDaoImpl dao, OrderDaoImpl orderDao,
+                               EmailSenderService emailSenderService,
+                               TrackedEmailOrder trackedEmailOrder) {
         super(dao);
-        this.orderDaol = orderDaol;
+        this.orderDao = orderDao;
         this.emailSenderService = emailSenderService;
         this.trackedEmailOrder = trackedEmailOrder;
     }
 
     @Override
-    @Transactional
     public void persist(Order order) {
         emailSenderService.sendSimpleEmail(trackedEmailOrder.trackedEmailOrderPersist(order));
-        orderDaol.persist(order);
+        orderDao.persist(order);
     }
 
     @Override
-    @Transactional
     public void update(Order order) {
         Mail mail = trackedEmailOrder.trackedEmailOrderUpdate(order);
         if (mail != null)
             emailSenderService.sendSimpleEmail(mail);
-        orderDaol.update(order);
+        orderDao.update(order);
     }
 
     @Override
-    @Transactional
     public void delete(Order order) {
         emailSenderService.sendSimpleEmail(trackedEmailOrder.trackedEmailOrderDelete(order));
-        orderDaol.delete(order);
+        orderDao.delete(order);
+    }
+
+    @Override
+    public void changeStatusToPaid(long order_id) {
+        Order order = this.getByKey(order_id);
+        order.setStatus(Status.PAID);
+        orderDao.update(order);
+    }
+
+    @Override
+    public void changeStatusToSent(long order_id) {
+        Order order = this.getByKey(order_id);
+        order.setStatus(Status.SENT);
+        orderDao.update(order);
+    }
+
+    @Override
+    public void changeStatusToDelivered(long order_id) {
+        Order order = this.getByKey(order_id);
+        order.setStatus(Status.DELIVERED);
+        orderDao.update(order);
+    }
+
+    @Override
+    public void changeStatusToCompleted(long order_id) {
+        Order order = this.getByKey(order_id);
+        order.setStatus(Status.COMPLETE);
+        orderDao.update(order);
     }
 }
