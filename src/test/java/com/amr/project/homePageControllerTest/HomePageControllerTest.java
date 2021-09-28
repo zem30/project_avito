37df358731fd7ba183e7fcd2df91610f13ef6573
Pavel Mezin <57784396+PaulMezoni@users.noreply.github.com>
2021-09-28 21:52:17 +0300

    create ApiDocumentation for HomePageController create HomePageControllerTest немного изменил application.properties(больше не ругается) поменял dependency в POM - v1.3

--- src/test/java/com/amr/project/homePageControllerTest/HomePageControllerTest.java
+++ src/test/java/com/amr/project/homePageControllerTest/HomePageControllerTest.java
@@ -4,12 +4,12 @@ import com.amr.project.AbstractIntegrationTest;
 import com.github.database.rider.core.api.dataset.DataSet;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DataSet(value = {"Item.xml", "Item-category.xml",
         "Category.xml", "Shop.xml", "Shop-item.xml",
@@ -20,10 +20,9 @@ public class HomePageControllerTest extends AbstractIntegrationTest {
 
     @Test
     public void getHomePageController() throws Exception {
-        mockMvc.perform(get("homepage"))
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/homepage"))
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
                 .andDo(print())
                 .andExpect(status().isOk());
-//                .andExpect(jsonPath("$.length()").value(6));
     }
 }
