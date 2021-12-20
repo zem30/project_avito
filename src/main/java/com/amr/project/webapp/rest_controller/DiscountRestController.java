ee82767a39faa1a17fb8b5cd30318c0b54fd4d88
Pavel Kozhaev <zxc321z9@gmail.com>
2021-12-20 10:05:54 +0300

    add methods: getAllDiscountByShipId, createDiscount, applyDiscount, findAllBuyersForShop

--- src/main/java/com/amr/project/webapp/rest_controller/DiscountRestController.java
+++ src/main/java/com/amr/project/webapp/rest_controller/DiscountRestController.java
@@ -37,6 +37,62 @@ public class DiscountRestController {
     private final DiscountMapper discountMapper;
     private final ShopMapper shopMapper;
 
+    @ApiOperation(value = "Получение списка скидок по id магазина")
+    @GetMapping("shop/{id}/discounts")
+    public ResponseEntity<List<DiscountDto>> getAllDiscountByShipId(@PathVariable("id") long id) {
+        return ResponseEntity.ok(discountService.findByShop(shopMapper.dtoToShop(shopService.getShopId(id))));
+    }
+
+    @ApiOperation(value = "Создание скидки")
+    @PostMapping("discounts")
+    public ResponseEntity<DiscountDto> createDiscount(@RequestBody DiscountDto discountDto) {
+        if ((discountDto.getMinOrder() == null || discountDto.getShopId() == null) &&
+                (discountDto.getPercentage() == null || discountDto.getFixedDiscount() == null)) {
+            return ResponseEntity.badRequest().build();
+        }
+        Discount discount = discountMapper.discountDtoToDiscount(discountDto);
+        discount.setShop(shopMapper.dtoToShop(shopService.getShopId(discountDto.getShopId())));
+        discount = discountService.saveD(discount);
+        return ResponseEntity.ok().body(discountMapper.discountToDiscountDto(discount));
+    }
+
+    @ApiOperation(value = "Добавление скидок пользователям")
+    @PostMapping("discounts/users")
+    public ResponseEntity<?> applyDiscount(@RequestBody DiscountDto discountDto) {
+        if ((discountDto.getMinOrder() == null || discountDto.getShopId() == null) &&
+                (discountDto.getPercentage() == null || discountDto.getFixedDiscount() == null)) {
+            return ResponseEntity.badRequest().build();
+        }
+        Discount discount = discountService.findByAllFields(discountDto.getMinOrder(), discountDto.getPercentage(),
+                discountDto.getFixedDiscount(),
+                shopMapper.dtoToShop(shopService.getShopId(discountDto.getShopId()))).orElse(null);
+
+        List<User> users = discountDto.getUsers().stream().map(userMapper::dtoToUser).collect(Collectors.toList());
+
+        users.forEach(u -> {
+            User user = userService.findByUsername(u.getUsername());
+            List<Discount> discounts = user.getDiscounts().stream()
+                    .filter(d -> !d.getShop().equals(discount.getShop()))
+                    .collect(Collectors.toList());
+            discounts.add(discount);
+            user.setDiscounts(discounts);
+            userService.update(user);
+        });
+        return ResponseEntity.ok().build();
+    }
+
+
+    @ApiOperation(value = "Список всех клиентов магазина")
+    @GetMapping("shop/{id}/buyers")
+    public ResponseEntity<List<UserDto>> findAllBuyersForShop(@PathVariable("id") long id) {
+        return ResponseEntity.ok(userService.findAllBuyersForShop(
+                        shopMapper.dtoToShop(shopService.getShopId(id)))
+                .stream()
+                .map(userMapper::userToDto)
+                .collect(Collectors.toList()));
+    }
+
+
     @ApiOperation(value = "Получаем всех пользователей с ролью \"Пользователь\"")
     @GetMapping("/userlist/all")
     public ResponseEntity<List<UserDto>> findAllUsersWithRoleUser() {
@@ -84,11 +140,11 @@ public class DiscountRestController {
     @ApiOperation(value = "Добавление скидки")
     @PostMapping("/userlist/addDiscount")
     public ResponseEntity<?> addDiscount(@RequestBody DiscountDto discountDto) {
-        if (discountDto.getShop() == null | discountDto.getUser() == null)
-            return ResponseEntity.badRequest().build();
+//        if (discountDto.getShop() == null | discountDto.getUser() == null)
+//            return ResponseEntity.badRequest().build();
         Discount discount = discountMapper.discountDtoToDiscount(discountDto);
-        discount.setShop(shopService.getShop(discountDto.getShop().getName()));
-        discount.setUser(userService.findByUsername(discountDto.getUser().getUsername()));
+//        discount.setShop(shopService.getShop(discountDto.getShop().getName()));
+//        discount.setUser(userService.findByUsername(discountDto.getUser().getUsername()));
         discountService.persist(discount);
         return ResponseEntity.ok().build();
     }
