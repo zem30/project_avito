00ccc4b86067b7f2873086fc7a13dc0eedd6a9b9
Pavel Kozhaev <zxc321z9@gmail.com>
2021-12-20 19:09:21 +0300

    change sout to Logger

--- src/main/java/com/amr/project/util/QueryResultWrapper.java
+++ src/main/java/com/amr/project/util/QueryResultWrapper.java
@@ -1,14 +1,18 @@
 package com.amr.project.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.persistence.TypedQuery;
 
 public class QueryResultWrapper {
+    private static final Logger LOGGER = LoggerFactory.getLogger(QueryResultWrapper.class);
 
     public static <T> T wrapGetSingleResult(TypedQuery<T> query) {
         try {
             return query.getSingleResult();
         } catch (Exception e) {
-            System.err.println(e.getMessage());
+            LOGGER.warn(e.getMessage());
             return null;
         }
     }
