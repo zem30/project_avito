c5a4d18ae35f2d84390e8f68448336fe27af4993
Pavel Kozhaev <zxc321z9@gmail.com>
2021-12-29 16:47:49 +0300

    add mapping for userId

--- src/main/java/com/amr/project/converter/AdvertMapper.java
+++ src/main/java/com/amr/project/converter/AdvertMapper.java
@@ -3,12 +3,14 @@ package com.amr.project.converter;
 import com.amr.project.model.dto.AdvertDto;
 import com.amr.project.model.entity.Advert;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 
-@Mapper(componentModel = "spring", uses = ImageMapper.class)
+@Mapper(componentModel = "spring", uses = {UserMapper.class, ImageMapper.class})
 public interface AdvertMapper {
-
+    @Mapping(target = "userId" , source ="user.id")
     AdvertDto advertToDto(Advert advert);
 
+    @Mapping(target = "user.id" , source ="userId")
     Advert dtoToAdvert(AdvertDto advertDto);
 }
