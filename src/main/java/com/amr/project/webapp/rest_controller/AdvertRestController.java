66dda71a87eee470539d0f9468333b3d50b7e08e
Pavel Kozhaev <zxc321z9@gmail.com>
2021-12-29 16:55:55 +0300

    update method addAdvert

--- src/main/java/com/amr/project/webapp/rest_controller/AdvertRestController.java
+++ src/main/java/com/amr/project/webapp/rest_controller/AdvertRestController.java
@@ -1,11 +1,8 @@
 package com.amr.project.webapp.rest_controller;
 
 import com.amr.project.converter.AdvertMapper;
-
 import com.amr.project.model.dto.AdvertDto;
 import com.amr.project.model.entity.Advert;
-
-import com.amr.project.model.entity.CartItem;
 import com.amr.project.service.abstracts.AdvertService;
 import com.amr.project.service.abstracts.CategoryService;
 import com.amr.project.service.abstracts.UserService;
@@ -13,17 +10,19 @@ import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.security.Principal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/adverts")
 @AllArgsConstructor
+@Validated
 public class AdvertRestController {
 
     private final AdvertService advertService;
@@ -33,26 +32,29 @@ public class AdvertRestController {
 
 
     @ApiOperation(value = "Добавление объявления пользователя")
-    @PostMapping("/advert/new")
-    public ResponseEntity<?> addAdvert(@RequestBody AdvertDto advertDto, Principal principal ){
+    @PostMapping()
+    public ResponseEntity<?> addAdvert(@Valid @RequestBody AdvertDto advertDto, Principal principal) {
         Advert advert = advertMapper.dtoToAdvert(advertDto);
-        if (advertDto.getName() == null)
+        if (advertDto.getName() == null) {
             return ResponseEntity.badRequest().body("empty advert");
-            advert.setUser(userService.findByUsername(principal.getName()));
-
+        }
+        advert.setCategories(Arrays.stream(advertDto.getCategoriesName())
+                .map(categoryService::getCategory)
+                .collect(Collectors.toList()));
+        advert.setUser(userService.findByUsername(principal.getName()));
         advertService.persist(advert);
 
         return ResponseEntity.ok().build();
     }
 
     @ApiOperation(value = "Обновляет объявления пользователя")
-    @PutMapping("/advert/update")
+    @PutMapping()
     public ResponseEntity<AdvertDto> updateItem(@RequestBody @NonNull AdvertDto advertDto) {
         Advert advert = advertMapper.dtoToAdvert(advertDto);
         if (advertDto.getName() == null || advertDto.getCategories() == null || advertDto.getCategoriesName() == null)
             return ResponseEntity.badRequest().build();
         advert.setId(advertDto.getId());
-        advert.setUser(userService.findByUsername(advertDto.getUser().getUsername()));
+        advert.setUser(userService.getUserId(advertDto.getId()));
         advert.setCategories(Arrays.stream(advertDto.getCategoriesName())
                 .map(categoryService::getCategory)
                 .collect(Collectors.toList()));
@@ -60,19 +62,29 @@ public class AdvertRestController {
         return ResponseEntity.ok().body(advertMapper.advertToDto(advert));
 
     }
-    @GetMapping("advert/{id}")
+
+    @GetMapping("{id}")
     public ResponseEntity<AdvertDto> getAdvert(@PathVariable @NonNull Long id) {
         Advert advert = advertService.getByKey(id);
         return ResponseEntity.ok().body(advertMapper.advertToDto(advert));
     }
 
-    @GetMapping("/adverts")
-    public List<AdvertDto> getAllAdverts(){
+    @GetMapping()
+    public List<AdvertDto> getAllAdverts() {
         return advertService.getAllAdverts();
     }
 
+    @ApiOperation(value = "Список объявлений пользователя")
+    @GetMapping("user/{id}")
+    public List<AdvertDto> getAllAdvertsByUser(@PathVariable @NonNull Long id) {
+        return advertService.getAllUser(userService.getUserId(id))
+                .stream()
+                .map(advertMapper::advertToDto)
+                .collect(Collectors.toList());
+    }
+
     @ApiOperation(value = "Удаляет объект Advert по id")
-    @DeleteMapping("advert/{id}")
+    @DeleteMapping("{id}")
     public ResponseEntity<?> deleteItem(@PathVariable @NonNull Long id) {
         Advert advert = advertService.getByKey(id);
         if (advert == null)
